5c8536318f5e6630a54e652f5b490ccaf3365cc7
slagroom <sl4gr00m@gmail.com>
2020-01-28 22:01:02 -0400

    Make a server

--- index.js
+++ index.js
@@ -1,7 +1,76 @@
-const Person = require('./person');
+const fs = require('fs');
+const http = require('http');
+const path = require('path');
 
-person1 = new Person('Thom', 30);
-person2 = new Person('Danielle', 64);
+const publicFilesPath = path.join(__dirname, 'public');
 
-console.log(person1.greeting());
-console.log(person2.greeting());
+function statusFromErr(err) {
+    if (err.code === 'ENOENT') {
+        return 404;
+    }
+    return 500;
+}
+
+const contentTypes = {
+    '.html': 'text/html',
+    '.js': 'text/javascript',
+    '.css': 'text/css',
+    '.json': 'application/json',
+    '.png': 'image/png',
+    '.jpg': 'image/jpeg',
+};
+
+function contentTypeFromExt(ext) {
+    const contentType = contentTypes[ext];
+    return contentType === undefined
+        ? 'application/octet-stream'
+        : contentType;
+}
+
+const server = http.createServer((req, res) => {
+
+    let contentPath = path.join(
+        publicFilesPath,
+        req.url === '/' ? 'index.html' : req.url);
+
+    const contentExt = path.extname(contentPath);
+    const contentType = contentTypeFromExt(contentExt);
+
+    fs.readFile(contentPath, (err, data) => {
+
+        if (err) {
+
+            const serve500 = () => {
+                console.log(err);
+                res.writeHead(500);
+                res.end();
+            }
+
+            const status = statusFromErr(err);
+            if (status === 404 && contentExt === '.html') {
+                fs.readFile(path.join(publicFilesPath, '404.html'), (err, data) => {
+                    if (err) {
+                        serve500();
+                        return;
+                    }
+                    res.writeHead(404, { 'Content-Type': 'text/html' });
+                    res.end(data);
+                });
+                return;
+            }
+
+            serve500();
+            return;
+
+        } else {
+            res.writeHead(200, { 'Content-Type': contentType });
+            res.end(data);
+        }
+
+    });
+});
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, null, null, () => {
+    console.log(`listening on port ${PORT}...`);
+});
